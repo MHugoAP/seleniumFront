1c31df0864126aa3b670265b034d4e7dfe036a40
Miguel Hugo <dragonmike6@gmail.com>
2022-08-31 10:15:52 -0500

    I used the login runner to generate a new structure for me

--- automatizacion_Front/src/test/java/com/speedup/qa/runners/loginRunner.java
+++ automatizacion_Front/src/test/java/com/speedup/qa/runners/loginRunner.java
@@ -1,9 +1,14 @@
 package com.speedup.qa.runners;
 
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-public class LoginRunner {
-    
+@CucumberOptions(features = "src/test/resources/features/login.feature"
+        , glue = "com.speedup.qa.stepdefinitions.loginStepDefinition"
+        , snippets = SnippetType.CAMELCASE)
+public class loginRunner {
+
 }
