53ead258ef190111b907bae0a12c481113029c7f
Miguel Hugo <dragonmike6@gmail.com>
2022-08-31 10:18:04 -0500

    we create a new class so that we can use it later

---
+++ automatizacion_Front/src/test/java/com/speedup/qa/stepdefinitions/loginStepDefinition.java
@@ -0,0 +1,22 @@
+package com.speedup.qa.stepdefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class loginStepDefinition {
+    @Given("^enter the portal Linkedin$")
+    public void enterThePortalLinkedin() {
+
+    }
+
+    @When("^enter the username and password$")
+    public void enterTheUsernameAndPassword() {
+
+    }
+
+    @Then("^will enter the web already with the user$")
+    public void willEnterTheWebAlreadyWithTheUser() {
+
+    }
+}
