98c7a6f0fcfe568434e612e573e59beb093b95d7
Miguel Hugo <dragonmike6@gmail.com>
2022-09-01 10:46:12 -0500

    some steps were performed so that the actor can execute the function

--- automatizacion_Front/src/test/java/com/speedup/qa/stepdefinitions/loginStepDefinition.java
+++ automatizacion_Front/src/test/java/com/speedup/qa/stepdefinitions/loginStepDefinition.java
@@ -1,12 +1,32 @@
 package com.speedup.qa.stepdefinitions;
 
+import com.speedup.qa.tasks.OpenBrowser;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actors.Cast;
+import net.serenitybdd.screenplay.actors.OnStage;
+import net.thucydides.core.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+import static net.serenitybdd.screenplay.actors.OnStage.setTheStage;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
 public class loginStepDefinition {
+    @Managed
+    private WebDriver myBrowser;
+
+    @Before
+    public void setUpOnstage() {
+        setTheStage(Cast.whereEveryoneCan(BrowseTheWeb.with(myBrowser)));
+        theActorCalled("Miguel Hugo");
+    }
+
     @Given("^enter the portal Linkedin$")
     public void enterThePortalLinkedin() {
+        OnStage.theActorInTheSpotlight().wasAbleTo(OpenBrowser.browser());
 
     }
 
